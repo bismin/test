c37c9941c05d0336b63e1318d0e4b3a2d986167f
BIZMIN <bismingeorge007@gmail.com>
2018-07-09 20:21:04 +0530

    first commit

--- test/items.py
+++ test/items.py
@@ -11,3 +11,33 @@ from scrapy.item import Item,Field
 class CraigslistSampleItem(Item):
 	title=Field()
 	link=Field() 
+
+
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item,Field
+
+
+class CraigslistSampleItem(Item):
+	title=Field()
+	link=Field() 
+
+
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item,Field
+
+
+class CraigslistSampleItem(Item):
+	title=Field()
+	link=Field() 
